b7915c9b978ef4da84046936e1c0750700f126a6
manuel <manueletm69@gmail.com>
2025-08-18 20:35:31 -0400

    Creacion de Proyecto API Rest Alura Challege Foro

---
+++ src/main/java/com/aluracursos/apirestchallengeforo/ApirestchallengeforoApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.apirestchallengeforo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestchallengeforoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestchallengeforoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/apirestchallengeforo/ApirestchallengeforoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.apirestchallengeforo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestchallengeforoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
